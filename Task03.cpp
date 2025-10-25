0e5cf3c6afc43588c2764db59641cc5d3050a1b9
Dua-Tariq0104 <f2025-0104@bnu.edu.pk>
2025-10-25 14:01:23 +0500

    Create Task03.cpp

---
+++ Task03.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c, middle;
+    cout << "Enter three numbers: ";
+    cin >> a >> b >> c;
+
+    if ((a > b && a < c) || (a < b && a > c))
+        middle = a;
+    else if ((b > a && b < c) || (b < a && b > c))
+        middle = b;
+    else
+        middle = c;
+
+    cout << "The middle value is: " << middle << endl;
+    return 0;
+}

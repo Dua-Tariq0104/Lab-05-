193c0f69db03701fffbf4e7f34822f84906fc320
Dua-Tariq0104 <f2025-0104@bnu.edu.pk>
2025-10-25 14:12:58 +0500

    Create Task04.cpp

---
+++ Task04.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int month, year, days;
+    cout << "Enter a month (1-12): ";
+    cin >> month;
+    cout << "Enter a year: ";
+    cin >> year;
+
+    bool leap;
+    if (year % 100 == 0)
+        leap = (year % 400 == 0);
+    else
+        leap = (year % 4 == 0);
+
+    if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+        days = 31;
+    else if (month == 4 || month == 6 || month == 9 || month == 11)
+        days = 30;
+    else if (month == 2)
+        days = leap ? 29 : 28;
+    else {
+        cout << "Invalid month" << endl;
+        return 0;
+    }
+
+    cout << days << " days" << endl;
+    return 0;
+}
